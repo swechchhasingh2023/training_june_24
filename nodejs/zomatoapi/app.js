8bbfd3c9679fd0571481ecede811ffcfd74ffc7f
Swechchha Singh <swechchhasingh2023@gmail.com>
2024-08-04 13:16:13 +0530

    api code

--- nodejs/zomatoapi/app.js
+++ nodejs/zomatoapi/app.js
@@ -1,15 +1,22 @@
 let express = require('express');
+// import express from 'express';
+let {dbConnect, getData} = require('./controller/dbController');
 let app = express();
-let port = 9110;
+let port = 9111;
 let cors = require('cors')
+
+
 
 app.use(cors())
 app.get('/', (req, res) =>{
     res.send('Hiii from Express')
 })
 
-app.get('/location', (req, res) =>{
-    res.send('Hiii from location')
+app.get('/location', async(req, res) =>{
+    let query = {}
+    let collection = "location"
+    let output = await getData(collection, query)
+    res.send(output)
 })
 
 app.get('/quicksearch', (req, res) =>{
@@ -17,6 +24,7 @@ app.get('/quicksearch', (req, res) =>{
 })
 
 app.listen(port, (err) =>{
+    dbConnect()
     if (err) throw err;
     console.log(`Server is running on port ${port}`)
 })
--- nodejs/zomatoapi/controller/dbController.js
+++ nodejs/zomatoapi/controller/dbController.js
@@ -1,1 +1,35 @@
-let mongo = require('mongodb');
+let mongo = require('mongodb')
+// import mongo from 'mongodb'
+let {MongoClient} = mongo;
+// let mongoUrl = "mongodb://127.0.0.1:27017";
+
+let mongoUrl = "mongodb+srv://swechchhasingh2017:3AygzMS5TnB5JX76@mern.mjg31yk.mongodb.net/?retryWrites=true&w=majority&appName=mern"
+
+let client = new MongoClient(mongoUrl);
+
+async function dbConnect(){
+    await client.connect()
+    console.log("Connection Succesful")
+}
+
+let db = client.db('demoNode');
+
+async function getData(colName,query){
+    let output = [];
+    try{
+        const cursor = db.collection(colName).find(query);
+        for await(const data of cursor){
+            output.push(data)
+        }
+        cursor.close();
+    } catch(err){
+        output.push({"Error":"Error in get Data"})
+    }
+
+    return output
+}
+
+module.exports= {
+    dbConnect,
+    getData
+}
